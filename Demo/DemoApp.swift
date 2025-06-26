621f33e5c7276822092a2c350b37fbbf0327b2a0
Nakul <aryanakul31@gmail.com>
2025-06-26 23:24:37 +0530

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Nakul Arya on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
